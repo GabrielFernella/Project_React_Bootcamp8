90112b27e49462da5c75f221776ba4b62c2c6524
Gabriel Fernella Garcia dos Santos <Gabrielfernella@users.noreply.github.com>
2020-03-20 15:27:17 -0300

    Config Prettier and ESlint

--- modulo05/src/App.js
+++ modulo05/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-
 function App() {
-  return (
-    <div className="App">
-      <h1>teste</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>teste</h1>
+        </div>
+    );
 }
 
 export default App;
